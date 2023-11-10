bf688ecc1e66c7e59355ef2e08a5c10bfbb68acc
loadthecode0 <artyaastha@gmail.com>
2023-11-10 20:11:03 +0530

    a7 init

---
+++ a7/myfiles/A7 Starter Code/A7 Starter Code/qna_tool.cpp
@@ -0,0 +1,145 @@
+#include <assert.h>
+#include <sstream>
+#include "qna_tool.h"
+
+using namespace std;
+
+QNA_tool::QNA_tool(){
+    // Implement your function here  
+}
+
+QNA_tool::~QNA_tool(){
+    // Implement your function here  
+}
+
+void QNA_tool::insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence){
+    // Implement your function here  
+    return;
+}
+
+Node* QNA_tool::get_top_k_para(string question, int k) {
+    // Implement your function here
+    return nullptr;
+}
+
+void QNA_tool::query(string question, string filename){
+    // Implement your function here  
+    std::cout << "Q: " << question << std::endl;
+    std::cout << "A: " << "Studying COL106 :)" << std::endl;
+    return;
+}
+
+std::string QNA_tool::get_paragraph(int book_code, int page, int paragraph){
+
+    cout << "Book_code: " << book_code << " Page: " << page << " Paragraph: " << paragraph << endl;
+    
+    std::string filename = "mahatma-gandhi-collected-works-volume-";
+    filename += to_string(book_code);
+    filename += ".txt";
+
+    std::ifstream inputFile(filename);
+
+    std::string tuple;
+    std::string sentence;
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Unable to open the input file " << filename << "." << std::endl;
+        exit(1);
+    }
+
+    std::string res = "";
+
+    while (std::getline(inputFile, tuple, ')') && std::getline(inputFile, sentence)) {
+        // Get a line in the sentence
+        tuple += ')';
+
+        int metadata[5];
+        std::istringstream iss(tuple);
+
+        // Temporary variables for parsing
+        std::string token;
+
+        // Ignore the first character (the opening parenthesis)
+        iss.ignore(1);
+
+        // Parse and convert the elements to integers
+        int idx = 0;
+        while (std::getline(iss, token, ',')) {
+            // Trim leading and trailing white spaces
+            size_t start = token.find_first_not_of(" ");
+            size_t end = token.find_last_not_of(" ");
+            if (start != std::string::npos && end != std::string::npos) {
+                token = token.substr(start, end - start + 1);
+            }
+            
+            // Check if the element is a number or a string
+            if (token[0] == '\'') {
+                // Remove the single quotes and convert to integer
+                int num = std::stoi(token.substr(1, token.length() - 2));
+                metadata[idx] = num;
+            } else {
+                // Convert the element to integer
+                int num = std::stoi(token);
+                metadata[idx] = num;
+            }
+            idx++;
+        }
+
+        if(
+            (metadata[0] == book_code) &&
+            (metadata[1] == page) &&
+            (metadata[2] == paragraph)
+        ){
+            res += sentence;
+        }
+    }
+
+    inputFile.close();
+    return res;
+}
+
+void QNA_tool::query_llm(string filename, Node* root, int k, string API_KEY, string question){
+
+    // first write the k paragraphs into different files
+
+    Node* traverse = root;
+    int num_paragraph = 0;
+
+    while(num_paragraph < k){
+        assert(traverse != nullptr);
+        string p_file = "paragraph_";
+        p_file += to_string(num_paragraph);
+        p_file += ".txt";
+        // delete the file if it exists
+        remove(p_file.c_str());
+        ofstream outfile(p_file);
+        string paragraph = get_paragraph(traverse->book_code, traverse->page, traverse->paragraph);
+        assert(paragraph != "$I$N$V$A$L$I$D$");
+        outfile << paragraph;
+        outfile.close();
+        traverse = traverse->right;
+        num_paragraph++;
+    }
+
+    // write the query to query.txt
+    ofstream outfile("query.txt");
+    outfile << "These are the excerpts from Mahatma Gandhi's books.\nOn the basis of this, ";
+    outfile << question;
+    // You can add anything here - show all your creativity and skills of using ChatGPT
+    outfile.close();
+ 
+    // you do not need to necessarily provide k paragraphs - can configure yourself
+
+    // python3 <filename> API_KEY num_paragraphs query.txt
+    string command = "python3 ";
+    command += filename;
+    command += " ";
+    command += API_KEY;
+    command += " ";
+    command += to_string(k);
+    command += " ";
+    command += "query.txt";
+
+    system(command.c_str());
+    return;
+}
